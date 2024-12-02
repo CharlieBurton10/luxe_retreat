1c4f234ced7a2245321b09b17e6f1e60984ca230
Charlie Burton <CharlotteMEBurton@hotmail.com>
2024-12-02 15:06:17 +0000

    Customised Admin

--- memberships/admin.py
+++ memberships/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from .models import Membership, Category
 
+class MembershipAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'description',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
 # Register your models here.
-admin.site.register(Membership)
-admin.site.register(Category)
+admin.site.register(Membership, MembershipAdmin)
+admin.site.register(Category, CategoryAdmin)
--- treatments/admin.py
+++ treatments/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 from .models import Treatment, Category
 
+class TreatmentAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'description',
+        'price',
+        'time',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
 # Register your models here.
-admin.site.register(Treatment)
-admin.site.register(Category)
+admin.site.register(Treatment, TreatmentAdmin)
+admin.site.register(Category, CategoryAdmin)
