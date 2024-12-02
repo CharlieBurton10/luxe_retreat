e61cb2c49794607beabdb14b14b718fd1c56e7b9
Charlie Burton <CharlotteMEBurton@hotmail.com>
2024-12-02 14:49:18 +0000

    Add membership and treatments app, models and fixtures

--- memberships/apps.py
+++ memberships/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MembershipConfig(AppConfig):
+class MembershipsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'membership'
+    name = 'memberships'
