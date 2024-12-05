42e2adce4cb7418dc4aa67f309273b64d3185c96
Charlie Burton <CharlotteMEBurton@hotmail.com>
2024-12-05 14:10:05 +0000

    Add Search function

--- treatments/views.py
+++ treatments/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Treatment
 
 # Create your views here.
@@ -7,9 +9,21 @@ def all_treatments(request):
     """ A view to show all treatments, including sorting and search queries """
 
     treatments = Treatment.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('treatments'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            treatments = treatments.filter(queries)
 
     context = {
         'treatments': treatments,
+        'search_term': query,
     }
 
     return render(request, 'treatments/treatments.html', context)
