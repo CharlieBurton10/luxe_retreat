be67635019095f10cfc2113d493714a23ec30b9f
Charlie Burton <CharlotteMEBurton@hotmail.com>
2024-12-12 14:04:15 +0000

    Add checkout views and templates

--- luxe_retreat/urls.py
+++ luxe_retreat/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('memberships/', include('memberships.urls')),
     path('treatments/', include('treatments.urls')),
     path('bag/', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
