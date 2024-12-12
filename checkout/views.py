b9ebd2c94218de982e7b02bca8d7c8d6602efb8f
Charlie Burton <CharlotteMEBurton@hotmail.com>
2024-12-12 14:56:56 +0000

    Add stripe element

--- checkout/views.py
+++ checkout/views.py
@@ -18,6 +18,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51QVDKcI8CkP3NIzzie68q36caHckUFjP1KPdosbY30d1PtFb8Aar3b7RrMqU28EUIqzZb2LGews71VTQHjQlvNAc004GZR6DU0',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
