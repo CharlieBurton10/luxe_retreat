42c23500ea7a5822c906961ee5394ab4efb4bf25
Charlie Burton <CharlotteMEBurton@hotmail.com>
2024-12-12 10:51:17 +0000

    Add toasts

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
+
+from treatments.models import Treatment
 
 # Create your views here.
 
@@ -10,13 +13,18 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping basket """
 
+    treatment = get_object_or_404(Treatment, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request,
+                        (f'Updated {treatment.name} '
+                        f'quantity to {bag[item_id]}'))
     else:
         bag[item_id] = quantity
+        messages.success(request, f'Added {treatment.name} to your bag')
 
     request.session['bag'] = bag
     return render(request, 'bag/bag.html')
@@ -24,13 +32,18 @@ def add_to_bag(request, item_id):
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
 
+    treatment = get_object_or_404(Treatment, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(request,
+                        (f'Updated {treatment.name} '
+                        f'quantity to {bag[item_id]}'))
     else:
         bag.pop(item_id)
+        messages.success(request, f'Removed {treatment.name} from your bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -39,10 +52,14 @@ def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
 
     try:
+        treatment = get_object_or_404(Treatment, pk=item_id)
         bag = request.session.get('bag', {})
         bag.pop(item_id)
+        messages.success(request, f'Removed {treatment.name} from your bag')
+
         request.session['bag'] = bag
         return HttpResponse(status=200)
     # If remove failed, throw an error
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
