25c891c6cf10cd5d46fbfe9acd69c03fd5cbf2bc
Charlie Burton <CharlotteMEBurton@hotmail.com>
2025-01-09 12:10:55 +0000

    Removed Secret Key and set debug

--- luxe_retreat/settings.py
+++ luxe_retreat/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n3c5fdwj227-194=sc2jb(3)yswctb24+rz#iil&p4c*)o614h'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['luxe-retreat-cf96daa6ac2f.herokuapp.com', 'localhost', '8000-charlieburt-luxeretreat-ohiu79p7nhv.ws.codeinstitute-ide.net',]
 #ALLOWED_HOSTS = ['8000-charlieburt-luxeretreat-ohiu79p7nhv.ws.codeinstitute-ide.net']
