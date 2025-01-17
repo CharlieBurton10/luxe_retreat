b49fb866b7a0db96bc6d97743a5cd7ab8db74503
Charlie Burton <CharlotteMEBurton@hotmail.com>
2025-01-17 11:28:58 +0000

    Edit base.css file

--- luxe_retreat/settings.py
+++ luxe_retreat/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG =  False #'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['luxe-retreat-cf96daa6ac2f.herokuapp.com', 'localhost', '8000-charlieburt-luxeretreat-ohiu79p7nhv.ws.codeinstitute-ide.net',]
